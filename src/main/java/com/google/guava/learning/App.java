97e18725df3185bbd506f8b43cc639c3770202ef
BHARAT SAVANI <savanibharat@gmail.com>
2015-05-17 14:43:01 -0700

    Repo for learning Google Guava

---
+++ src/main/java/com/google/guava/learning/App.java
@@ -0,0 +1,13 @@
+package com.google.guava.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
